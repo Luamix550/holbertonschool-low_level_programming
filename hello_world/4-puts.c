1e0b002b3b7642abaa5c889a537c803dc0a7d482
Luamix <luizalfonso611@gmail.com>
2023-10-29 14:54:56 -0500

    4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/**
+ * main - Entry
+ *
+ * Description Prints a string to stdout
+ * if return 0 (Success!)
+ */
 int main(void)
 {
     puts("\"Programming is like building a multilingual puzzle");
