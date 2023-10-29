a47292b4a17aa05f10da4626e92556eda271fa1d
Luamix <luizalfonso611@gmail.com>
2023-10-29 15:08:11 -0500

    5

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: Print a string to stdout
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,");
