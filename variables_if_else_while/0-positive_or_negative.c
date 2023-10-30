577822a54bfeea45fa636771bd3f23e003dc7ff0
Luamix <diazluisalfonso31@gmail.com>
2023-10-30 15:59:41 -0700

    0

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -11,9 +11,9 @@ int main(void)
     srand(time(0));
     n = rand() - RAND_MAX / 2;
     if (n > 0)
-    printf("is a positive number.");
+    printf("is positive");
     else if (n < 0)
-    printf("is a negative number.");
+    printf("is negative");
     else
     printf("is zero.");
     return (0);
