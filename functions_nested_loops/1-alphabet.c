3cc17e8e68e73544873767a134e760a0a94bf159
Luamix <luizalfonso611@gmail.com>
2023-10-31 18:48:46 -0500

    1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* print_alphabet - Print alphabet
+*
+*/
+void print_alphabet(void)
+{
+
+char alp;
+
+for (alp = '0'; alp <= 'z'; ++alp)
+{
+
+_putchar(alp);
+}
+_putchar('\n');
+}
