c9176dd42a958c1a538514689edc8a198f903388
Luamix <luizalfonso611@gmail.com>
2023-10-31 10:40:21 -0500

    3

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Program start.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+
+char alp;
+
+for (alp = 'a' ; alp <= 'z' ; alp++)
+{
+putchar(alp);
+}
+for (alp = 'A'; alp <= 'Z'; alp++)
+{
+putchar(alp);
+}
+putchar('\n');
+return (0);
+}
