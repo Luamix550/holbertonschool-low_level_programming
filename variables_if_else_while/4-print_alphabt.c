c42f4fa9e66015ff81f370aaf1bcdcb2afc55e86
Luamix <luizalfonso611@gmail.com>
2023-10-31 10:56:59 -0500

    4

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -10,13 +10,10 @@ int main(void)
 char alp;
 
 for (alp = 'a' ; alp <= 'z' ; alp++)
+if (alp != 'e' && alp != 'q')
 {
 putchar(alp);
 }
-for (alp = 'A'; alp <= 'Z'; alp++)
-{
-putchar(alp);
-}
-putchar('\n');
+putchar("\n");
 return (0);
 }
