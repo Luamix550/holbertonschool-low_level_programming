4d91803f503f56629803d7a869c7c30a65606baf
Luamix <luizalfonso611@gmail.com>
2023-10-31 14:32:22 -0500

    9

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Program start.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+
+int num;
+
+for (num = 48 ; num <= 57 ; num++)
+{
+putchar(num);
+putchar(',');
+}
+putchar('\n');
+return (0);
+}
