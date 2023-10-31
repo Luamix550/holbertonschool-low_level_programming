8743c0d9e247acbf1d223523d46993972fb474fb
Luamix <luizalfonso611@gmail.com>
2023-10-31 11:40:03 -0500

    6

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -6,11 +6,14 @@
  */
 int main(void)
 {
-int num;
-for (num = 48 ; num <= 57 ; num++)
+
+char alp;
+
+for (alp = 'z' ; alp >= 'a' ; alp--)
 {
-putchar(num);
+putchar(alp);
 }
 putchar('\n');
 return (0);
 }
+
