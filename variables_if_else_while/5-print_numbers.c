7df85ae1dcea3462517e87067dab7357886f4852
Luamix <luizalfonso611@gmail.com>
2023-10-31 11:16:17 -0500

    5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Program start.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+int num;
+for (num = 48 ; num <= 58 ; num++)
+{
+putchar(num);
+}
+putchar('\n');
+return (0);
+}
