ac001b17e683d7443cee3fac53ed41e28ad145d6
Luamix <luizalfonso611@gmail.com>
2023-11-01 15:16:39 -0500

    3

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,9 +1,7 @@
 #include "main.h"
 /**
  * _islower -: checks for lowercase character
- *  
  * c parameter : checks whether c is a lowercase letter or not
- * 
  * Return: 1 true
  *         0 false
  */
