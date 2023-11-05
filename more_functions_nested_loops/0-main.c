95320854e2598edcc47b2f2b6c0af2064b99e990
Luamix <luizalfonso611@gmail.com>
2023-11-05 12:22:43 -0500

    0-point

--- more_functions_nested_loops/0-main.c
+++ more_functions_nested_loops/0-main.c
@@ -1,18 +1,16 @@
-#include "main.h"
 #include <stdio.h>
+#include "main.h"
 
 /**
- * main - check the code.
+ * main - check the code for Holberton School students.
  *
  * Return: Always 0.
  */
 int main(void)
 {
-    char c;
+	char c;
 
-    c = 'A';
-    printf("%c: %d\n", c, _isupper(c));
-    c = 'a';
-    printf("%c: %d\n", c, _isupper(c));
-    return (0);
+	for (c = 'A'; c <= 'Z'; ++c)
+		printf("%c: %d\n", c, _isupper(c));
+	return (0);
 }
