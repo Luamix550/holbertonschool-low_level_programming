649d21b5b6e34a4e99ccce040b2cb3d0efec9a75
Luamix <luizalfonso611@gmail.com>
2023-11-04 19:08:06 -0500

    9 times table

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -7,22 +7,24 @@ void times_table(void)
 int row, column;
 int num;
 
-for (row = 0; row < 10; row++){
+for (row = 0; row < 10; row++)
+{
 _putchar('0');
-    for (column = 1; column < 10; column++) {
-          num = row * column;
-            _putchar(',');
-            _putchar(' ');
-            if (num < 10){
-                _putchar(' ');
-                _putchar((num % 10 ) + '0');
-            }
-            else{
-                _putchar((num / 10 ) + '0');    
-                _putchar((num % 10 ) + '0');
-            }
-            
-    }
-    _putchar('\n');
+for (column = 1; column < 10; column++)
+{
+num = row * column;
+_putchar(',');
+_putchar(' ');
+if (num < 10)
+{
+_putchar(' ');
+_putchar((num % 10 ) + '0');
+}
+else{
+_putchar((num / 10 ) + '0');    
+_putchar((num % 10 ) + '0');
+}
+}
+_putchar('\n');
 }
 }
