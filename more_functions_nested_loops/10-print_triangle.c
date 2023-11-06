ff1c7a97f165306c7458539497a28e12be568e11
Luamix <luizalfonso611@gmail.com>
2023-11-06 12:32:33 -0500

    10 triangle

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_triangle - prints a # depending on the number given to size
+ * @size: get a number
+ */
+void print_triangle(int size)
+{
+int i, j, k;
+
+for (i = 1; i <= size; ++i)
+{
+for (k = 1; k <= size - i; ++k)
+{
+_putchar(' ');
+}
+for (j = 1 ; j <= i ; ++j)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
