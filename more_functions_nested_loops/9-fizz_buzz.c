de6004b4d723f9feed96c731dc2d4d0f65b22408
Luamix <luizalfonso611@gmail.com>
2023-11-06 01:52:39 -0500

    fizzbuzz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -2,21 +2,26 @@
 
 int main()
 {
-    int i;
-    for (i = 1; i <= 100; i++)
-    {
-    if (i % 3 == 0 && i % 5 == 0){
-        printf("FizzBuzz\n");
-    }
-    else if (i % 3 == 0){
-        printf("Fizz\n");
-    }
-    else if (i % 5 == 0){
-        printf("Buzz\n");
-    }
-    else{
-        printf("%d\n",i);
-    }
-    }
-return (0);
+int i;
+for ( i = 1; i <= 100; i++)
+{
+if (i % 3 == 0 && i % 5 == 0)
+{
+printf("FizzBuzz ");
 }
+else if (i % 3 == 0)
+{
+printf("Fizz ");
+}
+else if (i % 5 == 0)
+{
+printf("Buzz ");
+} 
+else{
+printf("%d ", i);
+}
+}
+printf("\n");
+return 0;
+}
+
