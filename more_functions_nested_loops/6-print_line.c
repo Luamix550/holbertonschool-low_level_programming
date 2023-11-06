8953add1a5d74e4e1240560692d2f9703505aac3
Luamix <luizalfonso611@gmail.com>
2023-11-05 23:31:36 -0500

    6 print line

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *print_line - prints an underscore depending on the number given to n
+ *
+ * @n: get a number
+ */
+void print_line(int n)
+{
+if (n <= 0)
+{
+_putchar('\n');
+}
+else
+{
+int i;
+for (i = 0 ; i < n ; i++)
+{
+_putchar('_');
+}
+_putchar('\n');
+}
+}
