e4532bd34c05e0ca97c9abc9d79253fac8d4f79d
Luamix <luizalfonso611@gmail.com>
2023-11-09 11:34:54 -0500

    puts half 6

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -9,16 +9,13 @@ void puts_half(char *str)
     int len = 0;
     int i = 0;
 
-    // Calcular la longitud de la cadena
     while (str[len] != '\0') {
         len++;
     }
 
-    // Calcular el índice de inicio para imprimir la segunda mitad
     i = (len + 1) / 2;
 
     if (len > 0) {
-        // Imprimir la segunda mitad de la cadena si la longitud es mayor que 0
         while (str[i] != '\0') {
             _putchar(str[i]);
             i++;
