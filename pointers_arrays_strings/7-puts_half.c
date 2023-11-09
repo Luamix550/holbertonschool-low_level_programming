aaf06fb0d8dd6f6d8358fabe9b25b4ce61667058
Luamix <luizalfonso611@gmail.com>
2023-11-09 11:37:19 -0500

    puts half 7

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -6,20 +6,24 @@
  */
 void puts_half(char *str)
 {
-    int len = 0;
-    int i = 0;
+int len = 0;
+int i = 0;
 
-    while (str[len] != '\0') {
-        len++;
-    }
+while (str[len] != '\0')
+{
+len++;
+}
 
-    i = (len + 1) / 2;
+i = (len + 1) / 2;
 
-    if (len > 0) {
-        while (str[i] != '\0') {
-            _putchar(str[i]);
-            i++;
-        }
-    }
-    _putchar('\n');
+if (len > 0)
+{
+
+while (str[i] != '\0')
+{
+_putchar(str[i]);
+i++;
+}
+}
+_putchar('\n');
 }
