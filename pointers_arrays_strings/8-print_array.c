0fa783d0b27ee44a41dadc3a0def401cb66c7ad4
Luamix <luizalfonso611@gmail.com>
2023-11-09 13:49:25 -0500

    prints array

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - prints array
+ *
+ * @a: This pointer points to the first element of an array of integers.
+ * @n: represents the size of the array
+ */
+void print_array(int *a, int n)
+{
+int i;
+
+for (i = 0; i < n; i++)
+{
+if (i == n - 1)
+{
+printf("%d\n", a[i]);
+}
+else
+{
+printf("%d, ", a[i]);
+}
+}
+}
