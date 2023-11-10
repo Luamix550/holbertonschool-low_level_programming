324edccb0999efdafa0e634971a50e5f4a88dd66
Luamix <luizalfonso611@gmail.com>
2023-11-09 20:48:06 -0500

    atoi 2

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -6,23 +6,39 @@
 */
 int _atoi(char *s)
 {
-int sign = 1;
-int result = 0;
-int i = 0;
+	int i, d, n, len, f, digit;
 
+	i = 0;
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
 
-if (s[i] == '-')
-{
-sign = -1;
-i++;
-}
+	while (s[len] != '\0')
+		len++;
 
+	while (i < len && f == 0)
+	{
+		if (s[i] == '-')
+			++d;
 
-while (s[i] >= '0' && s[i] <= '9')
-{
-result = result * 10 + (s[i] - '0');
-i++;
-}
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			digit = s[i] - '0';
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
+		}
+		i++;
+	}
 
-return (sign * result);
+	if (f == 0)
+		return (0);
+
+	return (n);
 }
