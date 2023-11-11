0eb9f7a6c4b07152097457761895c4c3e50c5841
Luamix <luizalfonso611@gmail.com>
2023-11-10 20:36:55 -0500

    compare two strings

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,3 +1,6 @@
+#include <stdio.h>
+#include <string.h>
+
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _putchar(char c);
@@ -12,3 +15,4 @@ int _atoi(char *s);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
