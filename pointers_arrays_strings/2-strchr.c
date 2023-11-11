60cdca7f2440911a42e244f804fa50d46489c4e2
Luamix <luizalfonso611@gmail.com>
2023-11-11 15:22:50 -0500

    locates character 4

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -8,11 +8,16 @@
  */
 char *_strchr(char *s, char c)
 {
-    while (*s != '\0')
+    int i;
+    int length = strlen(s);
+
+    for (i = 0; i <= length; i++)
     {
-        if (*s == c)
-            return s;
-        s++;
+        if (s[i] == c || (c == '\0' && s[i] == '\0'))
+        {
+            return (&s[i]);
+        }
     }
+
     return NULL;
 }
