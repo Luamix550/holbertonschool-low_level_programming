3b4fce2ceb731b74ca2dc243ea7b12afe1c08cb9
Luamix <luizalfonso611@gmail.com>
2023-11-10 19:40:54 -0500

    copy string 2

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,19 +1,26 @@
 #include "main.h"
-
+/**
+ * *_strncpy - copy string
+ * @dest: get the string
+ * @src: pointer that has the string
+ * @n:number of times characters can be printed
+ * Return: tmp
+ */
 char *_strncpy(char *dest, char *src, int n)
 {
 
-char* tmp = dest;
+char *tmp = dest;
 
-while(*src && n--){
+while (*src && n--)
+{
 
-*dest++ = *src++; 
+*dest++ = *src++;
 
 }
 while (n > 0)
 {
-    *dest++ = '\0';
-    n--;
+*dest++ = '\0';
+n--;
 }
 
 return (tmp);
