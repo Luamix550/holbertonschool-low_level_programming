b3c894d77f078157e07307032426964471a58a33
Luamix <luizalfonso611@gmail.com>
2023-11-10 21:45:58 -0500

    rev string 2

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -1,17 +1,21 @@
 #include "main.h"
-#include <unistd.h>
-
+/**
+ * reverse_array - print array in rev
+ * @a: an array of integers
+ * @n: is the number of elements of the array
+ */
 void reverse_array(int *a, int n)
 {
 int i = 0;
 int j = n - 1;
 
-while (i < j){
-    int aux = a[i];
-    a[i] = a[j];
-    a[j] = aux;
+while (i < j)
+{
+int aux = a[i];
+a[i] = a[j];
+a[j] = aux;
 
-    i++;
-    j--;
+i++;
+j--;
 }
 }
