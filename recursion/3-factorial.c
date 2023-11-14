3134a64e96cf5c7cae5cf352ed2c04c6e39880a4
Luamix <luizalfonso611@gmail.com>
2023-11-14 12:09:28 -0500

    factored number

---
+++ recursion/3-factorial.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * factorial - factorial of n
+ * @n: number to be factored
+ * Return: returns the factored number
+*/
+int factorial(int n)
+{
+
+if (n < 1)
+{
+return (-1);
+}
+else if (n == 1)
+{
+return (1);
+}
+else
+{
+return (n * factorial(n - 1));
+}
+}
--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * _pow_recursion - returns the value of x raised to the power of y.
- * @x: raised.
+ * @x: raised
  * @y: power.
  * Return: return value of the x to the power of y.
 */
