e6df94afede3b5146c749f3d3583957732310036
Luamix <luizalfonso611@gmail.com>
2023-11-14 23:04:55 -0500

    static library

--- static_libraries/main.h
+++ static_libraries/main.h
@@ -1,18 +1,28 @@
-#include "main.h"
-/**
-  * _strcpy - Copy a string
-  * @dest: Destination value
-  * @src: Source value
-  * Return: the pointer to dest
-  */
-char *_strcpy(char *dest, char *src)
-{
-int i;
-for (i = 0; src[i] != '\0'; i++)
-{
-dest[i] = src[i];
-}
-dest[i++] = '\0';
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
-return (dest);
-}
+int _putchar(char c);
+int _islower(int c);
+int _isalpha(int c);
+int _abs(int n);
+int _isupper(int c);
+int _isdigit(int c);
+int _strlen(char *s);
+void _puts(char *s);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+
+#endif
