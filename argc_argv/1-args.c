842cbcc1c6485df5238b7065ae1b8a7b38151774
Luamix <luizalfonso611@gmail.com>
2023-11-15 22:24:14 -0500

    args 2

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -1,10 +1,13 @@
 #include <stdlib.h>
 #include <stdio.h>
-
+/**
+ * main - prints all arguments it receives
+ * @argc: total number of arguments
+ * @argv: contains the values of the arguments
+ * Return: 0 successful
+*/
 int main(int argc, char __attribute__((unused)) *argv[])
 {
-
-
-printf("%d\n",argc);
-return(0);
+printf("%d\n", argc - 1);
+return (0);
 }
