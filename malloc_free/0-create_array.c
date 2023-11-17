c0ca225c911b054729a1742168ebafe23255ed5f
Luamix <luizalfonso611@gmail.com>
2023-11-17 15:59:23 -0500

    create array 3

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -1,21 +1,18 @@
 #include <stdlib.h>
 
-char *create_array(unsigned int size, char c)
-{
-    unsigned int i; // Change to unsigned int for consistency with 'size'
+char *create_array(unsigned int size, char c) {
+    unsigned int i;
 
     if (size == 0)
         return NULL;
 
     char *arr = (char *)malloc(size * sizeof(char));
 
-    if (arr == NULL)
-    {
+    if (arr == NULL) {
         return NULL;
     }
 
-    for (i = 0; i < size; i++)
-    {
+    for (i = 0; i < size; i++) {
         arr[i] = c;
     }
 
