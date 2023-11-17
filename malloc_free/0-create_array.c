7d52b7d6592da9ebe217c4718a306db7f40cc43c
Luamix <luizalfonso611@gmail.com>
2023-11-17 12:23:51 -0500

    malloc

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+/**
+ *
+ * 
+*/
+char *create_array(unsigned int size, char c)
+{
+
+if(size == 0)
+    return (NULL);
+    
+    char *arr = (char *)malloc(size * sizeof(char));
+
+    if (arr == NULL)
+    {
+        return NULL;
+    }
+
+    for (unsigned int i = 0; i < size; i++)
+    {
+        arr[i] = c;
+    }
+    return (arr);
+}
---
+++ malloc_free/main.h
@@ -0,0 +1,5 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+char *create_array(unsigned int size, char c);
