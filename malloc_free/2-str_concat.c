cb0fafec0fdea27de0c749fb0aa003b7514d16c1
Luamix <luizalfonso611@gmail.com>
2023-11-17 20:04:16 -0500

    str concat 4

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -1,11 +1,6 @@
 #include "main.h"
 #include <string.h>
-/**
- * str_concat - concatenates two strings
- * @s2: first string 
- * @s1: second string
- * Return: string concatenates 
-*/
+
 char *str_concat(char *s1, char *s2)
 {
 char *new_str;
@@ -15,16 +10,16 @@ if(s1 == NULL)
 s1 = "";
 }
 
-if(s2 == NULL)
+if (s2 == NULL)
 {
 s2 = "";
 }
 
 new_str = malloc(strlen(s1)+strlen(s2)+1);
-if(new_str == NULL)
+if (new_str == NULL)
 {
-free(new_str);
-return(NULL);
+free (new_str);
+return (NULL);
 }
 
 new_str[0] = '\0';
