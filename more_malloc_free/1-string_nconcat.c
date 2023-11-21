b3905c3fdd49ee7cff254290d9cc7a4fa91f918b
Luamix <luizalfonso611@gmail.com>
2023-11-21 17:05:15 -0500

    string concat 6

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -8,44 +8,32 @@
 */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-    char *new_str = NULL;
-    unsigned int i;
-    char *aux = NULL;
+char *new_str = NULL;
+size_t s1_len, s2_len;
 
-    if (s1 == NULL)
-    {
-        s1 = "";
-    }
+if (s1 == NULL)
+{
+s1 = "";
+}
 
-    if (s2 == NULL)
-    {
-        s2 = "";
-    }
+if (s2 == NULL)
+{
+s2 = "";
+}
 
-    new_str = malloc(strlen(s1) + n + 1);
+s1_len = strlen(s1);
+s2_len = strlen(s2);
 
-    if (new_str == NULL)
-    {
-        return (NULL);
-    }
+if (n>= s2_len)
+{
+n = s2_len;
+}
 
-    aux = malloc(n + 1);
+new_str = (char *) malloc(s1_len + n + 1);
 
-    if (aux == NULL)
-    {
-        free(new_str);
-        return (NULL);
-    }
+strcpy(new_str, s1);
 
-    for (i = 0; i < n; i++)
-    {
-        aux[i] = s2[i];
-    }
-    aux[n] = '\0';
+strncat(new_str, s2, n);
 
-    strcpy(new_str, s1);
-    strcat(new_str, aux);
-    free(aux);
-
-    return (new_str);
+return (new_str);
 }
