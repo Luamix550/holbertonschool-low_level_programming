aaba63d1f5c26ca37559a06047e4023cb856f254
Luamix <luizalfonso611@gmail.com>
2023-11-24 17:57:51 -0500

    new dog

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -2,7 +2,13 @@
 #include <stdlib.h>
 #include <string.h>
 #include "dog.h"
-
+/**
+ * new_dog - creates a new dog
+ * @name: name od the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ * Return: return a new dog
+*/
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog;
