1fffb4e3f54a3f409d4f99b87de2504d0561c969
Luamix <luizalfonso611@gmail.com>
2023-11-24 17:45:54 -0500

    new dog

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct {
+    char *name;
+    float age;
+    char *owner;
+} dog_t;
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+    dog_t *newdog = (dog_t *)malloc(sizeof(dog_t));
+
+    if (newdog == NULL) {
+        return NULL;
+    }
+
+    newdog->name = strdup(name);
+
+    if (newdog->name == NULL) {
+        free(newdog);
+        return NULL;
+    }
+
+    newdog->age = age;
+    newdog->owner = strdup(owner);
+
+    if (newdog->owner == NULL) {
+        free(newdog->name);
+        free(newdog);
+        return NULL;
+    }
+
+    return newdog;
+}
