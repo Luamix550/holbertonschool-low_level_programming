ce93e9faef29160afdb17454c8461d735d0fe15c
Luamix <luizalfonso611@gmail.com>
2023-11-29 18:48:08 -0500

    print numbers

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,31 @@
+#include "variadic_functions.h"
+/**
+ * print_numbers - print numbers
+ * @separator: spaces and commas
+ * @n: numbers to print
+*/
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+
+va_list argument;
+unsigned int i;
+int num;
+
+va_start(argument, n);
+
+for (i = 0; i < n; i++)
+{
+
+num = va_arg(argument, int);
+
+printf("%d", num);
+
+if (separator != NULL && i < n - 1)
+{
+printf("%s", separator);
+}
+
+}
+va_end(argument);
+printf("\n");
+}
