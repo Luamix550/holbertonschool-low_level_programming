1615604f5d32986e163cad5eb33639800b7bb703
Luamix <luizalfonso611@gmail.com>
2023-12-11 21:51:10 -0500

    betty style

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,9 +1,8 @@
 #include "lists.h"
 /**
- * print_list - 
- * 
- * 
- * 
+ * print_list - function that prints all the elements of a list_t list.
+ * @h: pointer to struct
+ * Return: node counter
 */
 size_t print_list(const list_t *h)
 {
@@ -15,16 +14,16 @@ node_count = 0;
 
 while (ptr != NULL)
 {
-        if (ptr->str == NULL)
-        {
-            printf("[0] (nil)\n");
-        }
-        else
-        {
-            printf("[%d] %s\n", ptr->len, ptr->str);
-        }
-        ptr = ptr->next;
-        node_count++;
-        }
+if (ptr->str == NULL)
+{
+printf("[0] (nil)\n");
+}
+else
+{
+printf("[%d] %s\n", ptr->len, ptr->str);
+}
+ptr = ptr->next;
+node_count++;
+}
 return (node_count);
 }
