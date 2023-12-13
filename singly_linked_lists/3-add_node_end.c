faf5766e4f8d7462b3bbdbdc163732076f713de2
Luamix <luizalfonso611@gmail.com>
2023-12-13 16:06:16 -0500

    3 add node end

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -7,30 +7,30 @@
 */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new, *temp;
+list_t *new, *temp;
 
-	new = malloc(sizeof(list_t));
-	if (new == NULL)
-	{
-		return (NULL);
-	}
-	new->str = strdup(str);
-	new->len = strlen(str);
-	new->next = NULL;
+new = malloc(sizeof(list_t));
+if (new == NULL)
+{
+return (NULL);
+}
+new->str = strdup(str);
+new->len = strlen(str);
+new->next = NULL;
 
-	temp = *head;
-	if (*head == NULL)
-	{
-		*head = new;
-	}
-	else
-	{
-		while (temp->next)
-		{
-			temp = temp->next;
-		}
-		temp->next = new;
-	}
+temp = *head;
+if (*head == NULL)
+{
+*head = new;
+}
+else
+{
+while (temp->next)
+{
+temp = temp->next;
+}
+temp->next = new;
+}
 
-	return (new);
+return (new);
 }
