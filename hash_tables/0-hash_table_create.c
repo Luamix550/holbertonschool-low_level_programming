88699843be8b37dbd89be238ca5817ff1f6bf4d4
Luamix <luizalfonso611@gmail.com>
2023-12-18 16:36:23 -0500

    hash tables 3

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -3,26 +3,26 @@
  * hash_table_create - Creates a hash table
  * @size: The size of the array
  *
- * Return: A pointer to the newly created hash table, or NULL if an error occurs
+ * Return: A pointer to the newly created hash table
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
 hash_table_t *h_t = NULL;
 
 if (size == 0)
-return NULL;
+return (NULL);
 
 h_t = malloc(sizeof(hash_table_t));
 if (h_t == NULL)
-return NULL;
+return (NULL);
 
 h_t->array = malloc(size * sizeof(hash_node_t *));
 if (h_t->array == NULL)
 {
 free(h_t);
-return NULL;
+return (NULL);
 }
 
 h_t->size = size;
-return h_t;
+return (h_t);
 }
