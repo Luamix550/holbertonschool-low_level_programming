43df7c463b46f88da6f4afaab55df212d8227d38
Luamix <luizalfonso611@gmail.com>
2024-02-01 18:07:55 -0500

    taks 1

---
+++ search_algorithms/0-linear.c
@@ -0,0 +1,25 @@
+#include "search_algos.h"
+/**
+ * 
+ * 
+*/
+int linear_search(int *array, size_t size, int value)
+{
+
+size_t i;
+if (!value)
+{
+    return (-1);
+}
+
+for ( i = 0; i < size; i++)
+{
+    if (array[i] == value)
+    {
+        return (i);
+        break;
+    }
+    
+}
+return -1;
+}
