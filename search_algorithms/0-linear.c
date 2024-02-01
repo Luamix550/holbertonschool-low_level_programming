0507f559ed17c615747d0da12a021047ce8486f7
Luamix <luizalfonso611@gmail.com>
2024-02-01 18:16:11 -0500

    taks 1 vol 3

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -6,13 +6,13 @@
 int linear_search(int *array, size_t size, int value)
 {
 
-size_t i;
-if (value == NULL || array == NULL)
+size_t i = 0;
+if (array == NULL)
 {
     return (-1);
 }
 
-for ( i = 0; i < size; i++)
+for (; i < size; i++)
 {
     printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 
