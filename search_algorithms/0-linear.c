2184c330309ee9a0a387b029c240c498d75a19cc
Luamix <luizalfonso611@gmail.com>
2024-02-01 19:12:49 -0500

    fix betyt

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -1,7 +1,11 @@
 #include "search_algos.h"
 /**
- * 
- * 
+ * linear_search - algorithm where we start from one end and check
+ * every element of the list until the desired element is found.
+ * @array: is a pointer to the first element of the array to search in
+ * @size:  is the number of elements in array
+ * @value:  is the value to search for
+ * Return: -1 if not present or if array is NULL
 */
 int linear_search(int *array, size_t size, int value)
 {
